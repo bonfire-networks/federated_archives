cd8063348f96a7a7834b1ffcd3cb4893ffa653e9
Mayel de Borniol <git@mayel.space>
2025-02-13 23:50:58 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/978

--- deps.js.sh
+++ deps.js.sh
@@ -5,7 +5,7 @@
 # TODO: make this more configurable? ie. autogenerate from active extensions with JS assets
 
 DEPS='iconify_ex bonfire_ui_common bonfire_editor_milkdown'
-# 
+# bonfire_editor_quill bonfire_editor_ck
 
 chmod +x ./js-deps-get.sh
 ./js-deps-get.sh "$DEPS" $@
