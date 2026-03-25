6b3d6495922be4e0fbe1d30e7a653c013620b7c9
cranio_is_thinking <cranio_is_thinking@etna.foundation>
2026-03-25 22:28:51 +0000

    fix(dashboard): use ArchiveSearchLive with ConnectPandora; assign csrf_token in ArchiveSearchLive mount

--- priv/templates/lib/bonfire/web/views/dashboard_live.ex
+++ priv/templates/lib/bonfire/web/views/dashboard_live.ex
@@ -1,28 +1,8 @@
 defmodule Bonfire.Web.Views.DashboardLive do
   use Bonfire.UI.Common.Web, :surface_live_view
-  alias PanDoRa.API.Client
-  alias Bonfire.PanDoRa.Utils
   @behaviour Bonfire.UI.Common.LiveHandler
-
 
   on_mount {LivePlugs, [Bonfire.UI.Me.LivePlugs.LoadCurrentUser]}
 
-  def mount(_params, session, socket) do
-    csrf_token = session["_csrf_token"] || session["csrf_token"]
-    {:ok,
-      socket
-      |> assign(:page_title, "Search in your archive")
-      |> assign(:without_secondary_widgets, true)
-      |> assign(:term, nil)
-      |> assign(:csrf_token, csrf_token)
-    }
-  end
-
-  # Keep your existing handle_params implementation
-  def handle_params(%{"term" => term}, _, socket) do
-    {:noreply, assign(socket, term: term)}
-  end
-
-  def handle_params(_, _, socket), do: {:noreply, socket}
-
+  use Bonfire.PanDoRa.Web.ArchiveSearchLive, without_secondary_widgets: true
 end
