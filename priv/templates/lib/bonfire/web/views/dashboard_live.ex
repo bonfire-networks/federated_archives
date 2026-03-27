806a8e630dcbd25f362d5febac4a75c48448353b
cranio_is_thinking <alessandro@matango.tv>
2026-03-27 22:53:29 +0100

    fix(flavour): drop DashboardLive templates; use upstream default /dashboard

--- priv/templates/lib/bonfire/web/views/dashboard_live.ex
+++
@@ -1,8 +0,0 @@
-defmodule Bonfire.Web.Views.DashboardLive do
-  use Bonfire.UI.Common.Web, :surface_live_view
-  @behaviour Bonfire.UI.Common.LiveHandler
-
-  on_mount {LivePlugs, [Bonfire.UI.Me.LivePlugs.LoadCurrentUser]}
-
-  use Bonfire.PanDoRa.Web.ArchiveSearchLive, without_secondary_widgets: true
-end
